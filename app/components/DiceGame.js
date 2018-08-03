d27622154644dee3d83020252dad218f1d6c8aa9
Trevor <trevku155@gmail.com>
2018-08-03 02:07:35 -0400

    Created a GameState class to keep track of the DiceGame game state.

--- app/components/DiceGame.js
+++ app/components/DiceGame.js
@@ -3,15 +3,36 @@ import { NumberDisplayBoard } from './NumberDisplayBoard';
 import { DiceBoard } from './DiceBoard';
 import { InfoPanel } from './InfoPanel';
 
+class GameState {
+    constructor() {
+        // cost should always be a 5-digit number
+        this.product = {
+            name: null,
+            cost: null,
+            imgPath: null
+        };
+        // array of at most 4 values (integers, range 1-6) representing user rolls
+        this.rolls = []
+        // array of at most 4 values (booleans) representing whether the user guessed higher or lower (true = higher)
+        this.guesses = []
+
+        this.isGameStarted = false;
+        this.isRollPhase = false;
+        this.isGuessPhase = false;
+    }
+}
+
 export class DiceGame extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            game: new GameState()
         };
     }
 
     render() {
+        console.log(this.state);
+
         return (
             <div className="DiceGame">
                 <div className="left">
